80a01ca8100ce6590eb43965bfad1f895d1af935
elferia <a.t.qu.qu.qu@gmail.com>
2020-02-24 17:29:53 +0900

    rename

--- pybmsparser/parser.py
+++ pybmsparser/parser.py
@@ -13,11 +13,11 @@ Message = namedtuple('Message', 'channel message')
 
 @dataclass
 class BMS:
-    commandline: List[str] = dc.field(default_factory=list)
+    command: List[str] = dc.field(default_factory=list)
     message: List[Message] = dc.field(default_factory=lambda: [None] * 1000)
 
     def extend_commandline(self, _s, _loc, toks) -> None:
-        self.commandline.extend(toks.asList())
+        self.command.extend(toks.asList())
 
     def set_message(self, _s, _loc, toks) -> None:
         int16 = partial(int, base=16)
@@ -33,7 +33,7 @@ def parse(bms: str) -> BMS:
 
     def endif(): return pp.CaselessKeyword('endif') + wsp().suppress()
 
-    def command():
+    def definition():
         return (
             pp.Word(pp.alphanums) + (pp.Literal(' ') ^ '\t').suppress() +
             text())
@@ -43,14 +43,14 @@ def parse(bms: str) -> BMS:
             pp.Word(pp.nums, exact=3) + hex2() + pp.Literal(':').suppress() +
             pp.OneOrMore(hex2())).setParseAction(bms_obj.set_message)
 
-    def commandline():
+    def command():
         return (
             (wsp() + '#' + wsp()).suppress() +
-            pp.Group(endif() ^ command() ^ message()).
+            pp.Group(endif() ^ definition() ^ message()).
             setParseAction(bms_obj.extend_commandline))
 
     def comment(): return text().suppress()
-    def line(): return commandline() | comment()
+    def line(): return command() | comment()
 
     bms_obj = BMS()
     bmsparser = (
--- tests/test_pybmsparser.py
+++ tests/test_pybmsparser.py
@@ -9,42 +9,46 @@ def test_version():
 class TestBMS:
     def test_空文字列は0line(self):
         bms = parse('')
-        assert len(bms.commandline) == 0
+        assert len(bms.command) == 0
 
     def test_1行EOFは1line(self):
         bms = parse('#endif')
-        assert len(bms.commandline) == 1
+        assert len(bms.command) == 1
 
     def test_1行newlineは1line(self):
         bms = parse('#endif\n')
-        assert len(bms.commandline) == 1
+        assert len(bms.command) == 1
 
     def test_空行後1行は1line(self):
         bms = parse('\n#endif')
-        assert len(bms.commandline) == 1
+        assert len(bms.command) == 1
 
     def test_2行EOFは2line(self):
         bms = parse('#endif\n#endif')
-        assert len(bms.commandline) == 2
+        assert len(bms.command) == 2
 
     def test_2行newlineは2line(self):
         bms = parse('#endif\n#endif\n')
-        assert len(bms.commandline) == 2
+        assert len(bms.command) == 2
 
 
 class TestLine:
     def test_00(self):
         '#から始まるlineはcommandline'
         bms = parse('#endif')
-        assert len(bms.commandline) == 1
+        assert len(bms.command) == 1
 
     def test_01(self):
         '#から始まらないlineはcomment'
         bms = parse('foo')
-        assert len(bms.commandline) == 0
+        assert len(bms.command) == 0
 
 
 class TestCommandLine:
     def test_message(self):
+        bms = parse('#09910:20ff')
+        assert bms.message[99] == (0x10, [0x20, 0xff])
+
+    def test_definition(self):
         bms = parse('#09910:20ff')
         assert bms.message[99] == (0x10, [0x20, 0xff])
